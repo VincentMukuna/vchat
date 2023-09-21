d94c8c4dc5624452f5d5d695c0fa735562502b7c
Vincent Mukuna <mukunavincent28@gmail.com>
2023-09-21 23:29:56 +0300

    change function Id
    -To rfer to the new function with updated syntax

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -15,7 +15,7 @@ export const Server = {
   bucketIDGroupAvatars: "650501eb3c46e8cea2b0",
   bucketIDAttachments: "64d2846c6ba20730422c",
 
-  functionIDFuncs: "64fde861b589a24d786b",
+  functionIDFuncs: "664fde861b589a24d786b",
 
   documentIDGlobalChat: "650053648227be1533ad",
 };
