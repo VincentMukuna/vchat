dda85172c2b8537a0446acd797e12e0efd9afa69
Vincent Mukuna <mukunavincent28@gmail.com>
2023-10-06 21:59:28 +0300

    make dark the  initial color mode

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <>
-    <ColorModeScript initialColorMode="system" />
+    <ColorModeScript initialColorMode="dark" />
     <ChakraProvider resetCSS>
       <App />
     </ChakraProvider>
