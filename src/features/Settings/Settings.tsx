7d6e16c68ade2e32325081c2f92e0934d68b503b
Vincent Mukuna <mukunavincent28@gmail.com>
2023-10-28 19:01:24 +0300

    settings card improvements
    - made the whole card clickable instead of rendering a button inside it

--- src/features/Settings/Settings.tsx
+++ src/features/Settings/Settings.tsx
@@ -2,6 +2,7 @@ import {
   Card,
   FormControl,
   FormLabel,
+  Icon,
   IconButton,
   StackDivider,
   Switch,
@@ -12,6 +13,9 @@ import { ArrowRightOnRectangleIcon } from "@heroicons/react/20/solid";
 import { logUserOut } from "../../services/sessionServices";
 import { redirect, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
+
+import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
+import { slateDark } from "@radix-ui/colors";
 
 const Settings = () => {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -27,42 +31,41 @@ const Settings = () => {
       divider={<StackDivider />}
       className="flex flex-col p-4 transition-opacity"
     >
-      <div className="flex items-center w-full h-full max-w-sm p-3">
-        <div className="flex flex-col ">
+      <button
+        onClick={toggleColorMode}
+        className="flex items-center w-full h-full max-w-sm p-3"
+      >
+        <div className="flex flex-col items-start">
           <span className="">Toggle dark mode</span>
           <span className="text-sm italic dark:text-slate-300">
             Switch the apps theme
           </span>
         </div>
 
-        <Switch
-          className="ms-auto me-12"
-          colorScheme={colorMode === "dark" ? "facebook" : "blackAlpha"}
-          id="dark-mode"
-          onChange={toggleColorMode}
-          isChecked={colorMode === "dark"}
-        />
-      </div>
-      <div className="flex items-center w-full h-full max-w-sm p-3">
-        <div className="flex flex-col ">
+        {colorMode === "dark" ? (
+          <SunIcon className="w-6 h-6 ms-auto me-12" />
+        ) : (
+          <MoonIcon className="w-6 h-6 ms-auto me-12" />
+        )}
+      </button>
+      <button
+        onClick={() => {
+          setCurrentUser(null);
+          setCurrentUserDetails(null);
+          logUserOut();
+          navigate("../../login");
+        }}
+        className="flex items-center w-full h-full max-w-sm p-3"
+      >
+        <div className="flex flex-col items-start">
           <span className="">Log out</span>
           <span className="text-sm italic dark:text-slate-300">
             Delete this session
           </span>
         </div>
 
-        <IconButton
-          className="ms-auto me-12"
-          onClick={() => {
-            setCurrentUser(null);
-            setCurrentUserDetails(null);
-            logUserOut();
-            navigate("../../login");
-          }}
-          aria-label="log out"
-          icon={<ArrowRightOnRectangleIcon className="w-4 h-4 " />}
-        />
-      </div>
+        <ArrowRightOnRectangleIcon className="w-6 h-6 ms-auto me-12 " />
+      </button>
     </VStack>
   );
 };
