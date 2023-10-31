fc17c5885ebc1de1e6554569b0306322b068a56a
Vincent Mukuna <mukunavincent28@gmail.com>
2023-11-01 01:41:30 +0300

    ui fix

--- src/features/Room/Message.tsx
+++ src/features/Room/Message.tsx
@@ -3,7 +3,7 @@ import api from "../../services/api";
 import { SERVER } from "../../utils/config";
 import { useAuth } from "../../context/AuthContext";
 import { useChatsContext } from "../../context/ChatsContext";
-import { IChatMessage, IGroupMessage } from "../../interfaces";
+import { IChatMessage, IGroupMessage, IUserDetails } from "../../interfaces";
 import { DeleteIcon, PencilIcon } from "../../components/Icons";
 
 import useSWR, { mutate } from "swr";
@@ -20,6 +20,7 @@ import {
   Input,
   InputGroup,
   InputRightElement,
+  useDisclosure,
   useEditableControls,
 } from "@chakra-ui/react";
 
@@ -27,16 +28,20 @@ import { motion } from "framer-motion";
 import { CheckIcon } from "@heroicons/react/20/solid";
 import Blueticks from "../../components/Blueticks";
 import toast from "react-hot-toast";
+import UserProfileModal from "../Profile/UserProfileModal";
 
 interface MessageProps {
   message: IChatMessage | IGroupMessage;
   onDelete: (message: IChatMessage | IGroupMessage) => Promise<void>;
   i: number;
+  prev?: IChatMessage | IGroupMessage;
+  next?: IChatMessage | IGroupMessage;
 }
 
 const Message = forwardRef<any, MessageProps>(
-  ({ message, onDelete, i }, ref) => {
+  ({ message, onDelete, i, prev, next }, ref) => {
     const { currentUserDetails } = useAuth();
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
     const { selectedChat } = useChatsContext();
     if (!currentUserDetails || !selectedChat) return;
@@ -51,6 +56,8 @@ const Message = forwardRef<any, MessageProps>(
       message.$collectionId === SERVER.COLLECTION_ID_GROUP_MESSAGES
     );
     const mine = message.senderID === currentUserDetails.$id;
+    const prevSameSender = prev?.senderID === message.senderID;
+    const nextSameSender = next?.senderID === message.senderID;
 
     const { data: senderDetails } = useSWR(
       () => {
@@ -161,18 +168,28 @@ const Message = forwardRef<any, MessageProps>(
         onMouseLeave={() => setShowHoverCard(false)}
         ref={ref}
         tabIndex={0}
-        className={`relative   flex ${
+        className={`relative gap-1 flex ${
           mine ? "flex-row-reverse" : ""
         } items-end focus:outline-1 focus:outline-slate-600 transition-all`}
       >
         <Avatar
+          visibility={nextSameSender ? "hidden" : "visible"}
           src={mine ? currentUserDetails?.avatarURL : senderDetails?.avatarURL}
           name={
             mine ? currentUserDetails.name : (senderDetails?.name as string)
           }
           size="sm"
+          onClick={onOpen}
+          cursor={isGroupMessage ? "pointer" : ""}
         />
-        <div className="flex flex-col gap-1 mt-2">
+        {!mine && isGroupMessage && senderDetails && (
+          <UserProfileModal
+            isOpen={isOpen}
+            onClose={onClose}
+            user={senderDetails as IUserDetails}
+          />
+        )}
+        <div className="flex flex-col gap-1 mt-2 ">
           {attachments.length > 0 && (
             <AspectRatio maxW="250px" w={220} ratio={4 / 3}>
               <Image
@@ -185,12 +202,16 @@ const Message = forwardRef<any, MessageProps>(
           )}
           <div
             onClick={() => setIsEditing(true)}
-            className={`flex relative gap-3
-              ps-4  py-2 pe-4  ${
-                mine
-                  ? "bg-slate-300 dark:bg-gray4/90 dark:text-black rounded-br-none self-end "
-                  : "bg-slate-700 dark:bg-dark-tomato6  dark:text-dark-gray12 rounded-bl-none text-gray-100 "
-              } rounded-3xl w-fit max-w-[400px] break-words`}
+            className={`flex flex-col relative 
+                ps-4  pt-2 pb-1   ${
+                  mine
+                    ? "bg-slate-300 dark:bg-gray4/90 dark:text-black rounded-br-none self-end pe-3  gap-1"
+                    : "bg-dark-sky4/80 dark:bg-dark-sky4/95 dark:text-dark-gray12 rounded-bl-none text-gray-100 min-w-[8rem]"
+                } rounded-lg ${
+                  prevSameSender && (mine ? "rounded-t-md" : "rounded-t-sm")
+                }
+                ${nextSameSender && (mine ? "rounded-b-md" : "rounded-br-sm")} 
+                 w-fit max-w-[400px]  break-all `}
           >
             {mine ? (
               isEditing ? (
@@ -233,18 +254,15 @@ const Message = forwardRef<any, MessageProps>(
               </p>
             )}
             {mine && <Blueticks read={message.read} />}
-          </div>
-          <div
-            className={`flex  mx-3 text-[9px] tracking-wider md:text-[10px] gap-1 ${
-              mine
-                ? "dark:text-gray-500 justify-end text"
-                : "dark:text-gray-400 "
-            }`}
-          >
-            <span className="overflow-hidden text-elipsis whitespace-nowrap  max-w-[60px]  text-ellipsis ">
-              {!mine && (senderDetails?.name || "User ")}
-            </span>
-            {" " + getFormattedDateTime(message.$createdAt)}
+            <div
+              className={`flex self-end  mx-3 text-[0.54rem] tracking-wider gap-1 ${
+                mine
+                  ? "dark:text-gray-500 justify-end text"
+                  : "dark:text-gray-400 "
+              }`}
+            >
+              {" " + getFormattedDateTime(message.$createdAt)}
+            </div>
           </div>
         </div>
 
