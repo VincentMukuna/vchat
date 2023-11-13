74d2ea0455851c5fa081e2813c0ef2cae19f2b43
Vincent Mukuna <mukunavincent28@gmail.com>
2023-11-13 19:54:36 +0300

    [refactor] get paginated users

--- src/features/Groups/AddMembers.tsx
+++ src/features/Groups/AddMembers.tsx
@@ -1,5 +1,6 @@
 import {
   Avatar,
+  AvatarGroup,
   Button,
   Checkbox,
   ModalBody,
@@ -7,49 +8,70 @@ import {
   ModalFooter,
   ModalHeader,
   Stack,
+  VStack,
   useCheckboxGroup,
   useColorMode,
   useModalContext,
 } from "@chakra-ui/react";
-import { IGroup } from "../../interfaces";
-import useSWR, { mutate } from "swr";
+import { IGroup, IUserDetails } from "../../interfaces";
+import useSWR, { mutate, useSWRConfig } from "swr";
 import {
   editMembers,
   getGroupDetails,
 } from "../../services/groupMessageServices";
-import { blueDark, gray } from "@radix-ui/colors";
-import { getUsers } from "../../services/userDetailsServices";
-import { useEffect } from "react";
+import { blueDark, gray, slateDark } from "@radix-ui/colors";
+import { getUsers, searchUsers } from "../../services/userDetailsServices";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
+import { useInfinite } from "../../hooks/useInfinite";
+import Search from "../../components/Search";
+import { UserIcon } from "@heroicons/react/20/solid";
+import { motion } from "framer-motion";
+import { useAuth } from "../../context/AuthContext";
+import User, { UserAvatar, UserDescription } from "../UsersList/User";
 
 const AddMembers = ({ group }: { group: IGroup }) => {
+  const { currentUserDetails } = useAuth();
+  if (!currentUserDetails) return null;
   const { data: roomDetails } = useSWR(`details ${group.$id}`, () =>
     getGroupDetails(group.$id),
   );
-  const { data: users } = useSWR("users", async () => {
-    const [users] = await getUsers();
-    return users;
-  });
-  const { value, getCheckboxProps, setValue } = useCheckboxGroup();
+  const { cache } = useSWRConfig();
+  const { data, isLoading, error, size, setSize, isValidating, totalRef } =
+    useInfinite<IUserDetails>(getUsers, "users", /users-(\w+)/, []);
+
+  const users = data ? ([] as IUserDetails[]).concat(...data) : [];
   const { colorMode } = useColorMode();
 
   const { onClose } = useModalContext();
+
+  const [newMembers, setNewMembers] = useState<IUserDetails[]>([]);
 
   let memberIDs = roomDetails?.members.map(
     (member: any) => member.$id,
   ) as string[];
 
   const handleAddMembers = () => {
-    toast.promise(
-      editMembers(group.$id, (value as string[]).concat(memberIDs)),
+    const details = cache.get(`details ${group.$id}`)?.data;
+    mutate(
+      `details ${group.$id}`,
       {
-        loading: "Saving changes",
-        success: "Members changed",
-        error: "Something went wrong",
+        ...details,
+        members: [...newMembers, ...(roomDetails?.members as IUserDetails[])],
       },
+      { revalidate: false },
     );
-    mutate(group.$id);
     onClose();
+    editMembers(
+      group.$id,
+      newMembers.map((member) => member.$id).concat(memberIDs),
+    )
+      .then((newDoc) => {
+        mutate(`details ${group.$id}`, newDoc, { revalidate: false });
+      })
+      .catch(() => {
+        toast.error("Something went wrong");
+      });
   };
 
   return (
@@ -60,24 +82,132 @@ const AddMembers = ({ group }: { group: IGroup }) => {
         <p className="mb-2 text-sm italic text-dark-gray9">
           Check member to add to {group.name}
         </p>
-        <Stack maxH={200} overflowY={"auto"} gap={2}>
-          {users
-            ?.filter((user) => !memberIDs?.includes(user.$id))
-            .map((user) => {
+        <div>
+          <span className="font-bold dark:text-dark-slate11 text-dark-gray7">
+            Add Members
+          </span>
+          <AvatarGroup max={4}>
+            {newMembers.map((member) => {
               return (
-                <Checkbox
-                  key={user.$id}
-                  iconColor={blueDark.blue1}
-                  {...getCheckboxProps({ value: user.$id })}
-                >
-                  <div className="flex items-center gap-2 text-[12]">
-                    <Avatar name={user.name} src={user.avatarURL} size={"sm"} />
-                    {user.name}
-                  </div>
-                </Checkbox>
+                <Avatar
+                  src={member.avatarURL}
+                  icon={<UserIcon className="w-[26px] h-[26px]" />}
+                  size="md"
+                  key={member.$id}
+                />
               );
             })}
-        </Stack>
+          </AvatarGroup>
+
+          <VStack
+            maxWidth={"100%"}
+            minH={300}
+            maxH={400}
+            overflowY={"auto"}
+            overflowX={"hidden"}
+            borderWidth={1}
+            px={2}
+            pt={4}
+            bg={colorMode === "light" ? gray.gray3 : slateDark.slate2}
+            rounded={"md"}
+            mt={2}
+            spacing={0}
+          >
+            <Search
+              handleSearch={async (name, onCloseSearch) => {
+                let res = await searchUsers(name);
+                return res
+                  .filter(
+                    (user) =>
+                      user.$id !== currentUserDetails.$id &&
+                      !newMembers.some((member) => member.$id !== user.$id) &&
+                      !roomDetails?.members.some(
+                        (member) => (member as IUserDetails).$id === user.$id,
+                      ),
+                  )
+                  .map((user) => (
+                    <User
+                      user={user}
+                      onClick={() => {
+                        setNewMembers((prev) => {
+                          return [...prev, user];
+                        });
+
+                        onCloseSearch();
+                      }}
+                      key={user.$id}
+                    >
+                      <UserAvatar />
+                      <UserDescription />
+                    </User>
+                  ));
+              }}
+            />
+            {!isLoading &&
+              users?.length &&
+              ([] as IUserDetails[])
+                .concat(...users)
+                .filter(
+                  (user) =>
+                    user.$id !== currentUserDetails.$id &&
+                    !roomDetails?.members.some(
+                      (member) => (member as IUserDetails).$id === user.$id,
+                    ),
+                )
+                .map((user: IUserDetails, index) => {
+                  return (
+                    <div
+                      className="flex items-center w-full gap-2 group "
+                      key={user.$id}
+                    >
+                      <Checkbox
+                        type="checkbox"
+                        id={index.toString()}
+                        isChecked={newMembers.some(
+                          (member) => member.$id === user.$id,
+                        )}
+                        onChange={() => {
+                          if (newMembers.includes(user)) {
+                            setNewMembers((prev) => {
+                              return prev.filter(
+                                (member) => member.$id !== user.$id,
+                              );
+                            });
+                          } else {
+                            setNewMembers((prev) => {
+                              return [...prev, user];
+                            });
+                          }
+                        }}
+                      />
+                      <label
+                        htmlFor={index.toString()}
+                        className="flex items-center w-full gap-1 p-1 text-sm font-semibold tracking-wide cursor-pointer"
+                      >
+                        <User user={user} onClick={() => {}}>
+                          <UserAvatar size="sm" />
+                          <UserDescription />
+                        </User>
+                      </label>
+                    </div>
+                  );
+                })}
+
+            {totalRef.current >
+              ([] as IUserDetails[]).concat(...users!).length && (
+              <Button
+                variant={"ghost"}
+                onClick={() => {
+                  setSize(size + 1);
+                }}
+                isLoading={isValidating}
+                w={"full"}
+              >
+                {isValidating ? "Fetching" : "See more"}
+              </Button>
+            )}
+          </VStack>
+        </div>
       </ModalBody>
       <ModalFooter className="gap-2">
         <Button variant={"ghost"} width={"40"} onClick={onClose}>
@@ -89,7 +219,7 @@ const AddMembers = ({ group }: { group: IGroup }) => {
           onClick={handleAddMembers}
           color={colorMode === "dark" ? gray.gray2 : gray.gray1}
           title="save changes"
-          isDisabled={value.length < 1}
+          isDisabled={newMembers.length < 1}
         >
           Save
         </Button>
