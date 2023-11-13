c13f88ab8390761626a785ff923deeb90a22110a
Vincent Mukuna <mukunavincent28@gmail.com>
2023-11-14 00:04:59 +0300

    added clear group messages menu opt

--- src/features/Room/RoomActions.tsx
+++ src/features/Room/RoomActions.tsx
@@ -1,78 +1,134 @@
-import { MenuItem, MenuList } from "@chakra-ui/react";
+import { Button, MenuDivider, MenuItem, MenuList } from "@chakra-ui/react";
 import { Alert, confirmAlert } from "../../components/Alert/alertStore";
 import { useChatsContext } from "../../context/ChatsContext";
 import { clearChatMessages } from "../../services/chatMessageServices";
 import toast from "react-hot-toast";
 import { openModal } from "../../components/Modal";
-import EditMembers from "../Groups/EditMembers";
+import EditMembers from "../Groups/Actions/EditMembers";
 import { IGroup } from "../../interfaces";
-import AddMembers from "../Groups/AddMembers";
+import AddMembers from "../Groups/Actions/AddMembers";
 import { mutate, useSWRConfig } from "swr";
 import { unstable_serialize } from "swr/infinite";
+import { useAuth } from "../../context/AuthContext";
+import { slateDark } from "@radix-ui/colors";
+import EditGroupAdmins from "../Groups/Actions/EditGroupAdmins";
+import {
+  PencilIcon,
+  TrashIcon,
+  UserMinusIcon,
+  UserPlusIcon,
+} from "@heroicons/react/20/solid";
+import { clearGroupMessages } from "../../services/groupMessageServices";
 
-const RoomActions = ({ id, isGroup }: { id: string; isGroup: boolean }) => {
+const RoomActions = () => {
   const { selectedChat } = useChatsContext();
   const { cache } = useSWRConfig();
+
+  const { currentUserDetails } = useAuth();
+
+  if (!currentUserDetails) return null;
   if (!selectedChat) return null;
-  const chatMessagesKey = unstable_serialize(
-    () => selectedChat.$id + "-messages",
-  );
+
+  const isGroup = !!(selectedChat?.$collectionId === "groups");
+  const isAdmin =
+    isGroup && (selectedChat as IGroup).admins.includes(currentUserDetails.$id);
+
+  async function handleClearRoomMessages() {
+    const chatMessagesKey = unstable_serialize(
+      () => selectedChat!.$id + "-messages",
+    );
+    const messages = cache.get(chatMessagesKey)?.data;
+
+    (isGroup
+      ? clearGroupMessages(selectedChat.$id)
+      : clearChatMessages(selectedChat!.$id)
+    )
+      .then(() => {
+        mutate(chatMessagesKey, [[]], { revalidate: false });
+        mutate(`lastMessage ${selectedChat!.$id}`, undefined, {
+          revalidate: false,
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+        toast.error("Something went wrong");
+        mutate(chatMessagesKey, messages, { revalidate: false });
+        mutate(
+          `lastMessage ${selectedChat!.$id}`,
+          [].concat(...messages).at(0),
+          {
+            revalidate: false,
+          },
+        );
+      });
+  }
+
   return (
-    <MenuList className="px-2">
-      {!isGroup && (
+    <MenuList _dark={{ bg: slateDark.slate1 }}>
+      {(!isGroup || isAdmin) && (
         <>
           <MenuItem
-            py={"2.5"}
+            icon={<TrashIcon className="w-4 h-4" />}
+            py={"1"}
+            bg={"transparent"}
             onClick={() => {
               confirmAlert({
                 title: "Delete chat messages",
-                message: `Are you sure you want to delete all messages in this Chat? This action is irreversible`,
+                message: `Are you sure you want to delete all messages in this Conversation? This action is irreversible`,
                 confirmText: "Yes, delete all messages",
                 cancelText: "Keep messages",
                 onConfirm: () => {
-                  const messages = cache.get(chatMessagesKey)?.data;
-                  mutate(chatMessagesKey, [[]], { revalidate: false });
-                  mutate(`lastMessage ${selectedChat.$id}`, undefined, {
-                    revalidate: false,
-                  });
-                  let ps = clearChatMessages(id);
-                  ps.catch(() => {
-                    toast.error("Something went wrong");
-                    mutate(chatMessagesKey, messages, { revalidate: false });
-                    mutate(
-                      `lastMessage ${selectedChat.$id}`,
-                      [].concat(...messages).at(0),
-                      {
-                        revalidate: false,
-                      },
-                    );
-                  });
+                  handleClearRoomMessages();
                 },
               });
             }}
           >
             Clear Messages
           </MenuItem>
+          <MenuDivider />
         </>
       )}
       {isGroup && (
         <>
           <MenuItem
-            py={"2.5"}
-            onClick={() =>
-              openModal(<EditMembers group={selectedChat as IGroup} />)
-            }
-          >
-            Edit Members
-          </MenuItem>
-          <MenuItem
-            py={"2.5"}
+            py={"1"}
             onClick={() =>
               openModal(<AddMembers group={selectedChat as IGroup} />)
             }
+            bg={"transparent"}
+            icon={<UserPlusIcon className="w-4 h-4" />}
           >
             Add Members
           </MenuItem>
+          <MenuDivider />
+          <MenuItem
+            py={"1"}
+            onClick={() =>
+              openModal(<EditMembers group={selectedChat as IGroup} />)
+            }
+            bg={"transparent"}
+            icon={<UserMinusIcon className="w-4 h-4" />}
+          >
+            Remove Members
+          </MenuItem>
+
+          {isAdmin && (
+            <>
+              <MenuDivider />
+              <MenuItem
+                py={"1"}
+                bg={"transparent"}
+                onClick={() =>
+                  openModal(
+                    <EditGroupAdmins selectedGroup={selectedChat as IGroup} />,
+                  )
+                }
+                icon={<PencilIcon className="w-4 h-4" />}
+              >
+                Edit Admins
+              </MenuItem>
+            </>
+          )}
         </>
       )}
     </MenuList>
