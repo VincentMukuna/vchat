7c89d35781590e15a2ecd271d47c0b21b28b1cca
Vincent Mukuna <mukunavincent28@gmail.com>
2023-11-14 10:54:57 +0300

    [fix] copy

--- src/services/dateServices.ts
+++ src/services/dateServices.ts
@@ -9,7 +9,7 @@ export function getFormatedDate(timestamp: string) {
   let date = new Date(timestamp);
 
   if (isToday(date)) {
-    return format(date, "HH:mm pp 'today'");
+    return format(date, "p 'today'");
   }
   if (isYesterday(date)) {
     return "Yesterday";
