7a175d4a8e4014e145ad131d920e7919569872df
Vincent Mukuna <mukunavincent28@gmail.com>
2024-02-07 16:25:18 +0300

    [refactor] add common variables to context

--- src/context/RoomContext.tsx
+++ src/context/RoomContext.tsx
@@ -1,11 +1,21 @@
 import { createContext, useContext, useState } from "react";
-import { DirectMessageDetails, GroupMessageDetails } from "../interfaces";
+import {
+  DirectMessageDetails,
+  GroupMessageDetails,
+  IUserDetails,
+} from "../interfaces";
+import { SERVER } from "../utils/config";
+import { useAuth } from "./AuthContext";
+import { useChatsContext } from "./ChatsContext";
 
 type ContextMessage = DirectMessageDetails | GroupMessageDetails;
 
 interface RoomContextData {
   selectedMessages: ContextMessage[];
   setSelectedMessages: React.Dispatch<React.SetStateAction<ContextMessage[]>>;
+  isGroup: boolean;
+  isPersonal: boolean;
+  roomMessagesKey?: string;
 }
 
 export const RoomContext = createContext<RoomContextData | null>(null);
@@ -14,10 +24,28 @@ export const RoomProvider = ({ children }: { children: React.ReactNode }) => {
   const [selectedMessages, setSelectedMessages] = useState<ContextMessage[]>(
     [],
   );
+  const { selectedChat } = useChatsContext();
+  const { currentUserDetails } = useAuth();
+  const isGroup = !!(
+    selectedChat?.$collectionId === SERVER.COLLECTION_ID_GROUPS
+  );
+
+  const roomMessagesKey = `${selectedChat?.$id}-messages`;
+
+  const isPersonal =
+    selectedChat &&
+    !isGroup &&
+    selectedChat?.participants.every(
+      (participant: IUserDetails) =>
+        participant.$id === currentUserDetails?.$id,
+    );
 
   const contextData = {
     selectedMessages: selectedMessages,
     setSelectedMessages,
+    isGroup,
+    isPersonal,
+    roomMessagesKey,
   };
 
   return (
