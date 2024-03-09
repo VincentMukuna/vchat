eac8768ff903e91f9974f1cf5ef3a3cc0bad7e16
Vincent Mukuna <mukunavincent28@gmail.com>
2024-03-09 19:18:35 +0300

    [refactor] add relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: { chunkSizeWarningLimit: 1600 },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 });
