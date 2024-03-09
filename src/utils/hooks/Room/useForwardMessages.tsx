cd8f4e58788730d6e138c0d588eae34873502686
Vincent Mukuna <mukunavincent28@gmail.com>
2024-03-09 21:25:06 +0300

    Delete useForwardMessages.tsx

--- src/utils/hooks/Room/useForwardMessages.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import { useRoomContext } from "../../../context/RoomContext";
-
-export default function useForwardMessages() {
-  const [showChats, setShowChats] = useState(false);
-  const { selectedMessages } = useRoomContext();
-
-  function forwardSelectedMessages() {}
-}
