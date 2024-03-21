c544580b44275f8b0352f91b52263bb5dec49c16
Vincent Mukuna <mukunavincent28@gmail.com>
2024-03-21 03:26:26 +0300

    [fix] password input
    showing two icons

--- src/components/PasswordInput.tsx
+++ src/components/PasswordInput.tsx
@@ -1,14 +1,5 @@
-import {
-  IconButton,
-  Input,
-  InputGroup,
-  InputRightElement,
-} from "@chakra-ui/react";
-import { EyeIcon, EyeSlashIcon } from "@heroicons/react/20/solid";
-import React, { useRef } from "react";
-import usePassword from "../utils/hooks/usePassword";
-import { tomatoDark } from "@radix-ui/colors";
-import { motion } from "framer-motion";
+import { Input, InputGroup } from "@chakra-ui/react";
+import { useRef } from "react";
 
 interface PasswordInputProps {
   value: string;
@@ -16,7 +7,6 @@ interface PasswordInputProps {
 }
 
 const PasswordInput = ({ value, onChange }: PasswordInputProps) => {
-  const { show, toggle } = usePassword();
   const inputRef = useRef(null);
   return (
     <InputGroup>
@@ -28,24 +18,8 @@ const PasswordInput = ({ value, onChange }: PasswordInputProps) => {
         id="password"
         value={value}
         onChange={onChange}
-        type={show ? "text" : "password"}
+        type="password"
       />
-      <InputRightElement cursor={"pointer"}>
-        <IconButton
-          as={motion.button}
-          whileHover={{ scale: 1.01 }}
-          whileTap={{ scale: 0.98 }}
-          aria-label="show password"
-          onClick={() => toggle()}
-          bg={"none"}
-        >
-          {show ? (
-            <EyeSlashIcon className="w-4 h-4" />
-          ) : (
-            <EyeIcon className="w-4 h-4" />
-          )}
-        </IconButton>
-      </InputRightElement>
     </InputGroup>
   );
 };
