7d7f04af049c8bcc0f7b91c375216599a3e60c2f
Vincent Mukuna <mukunavincent28@gmail.com>
2024-03-21 03:27:31 +0300

    [feat] add system messages to rooms

---
+++ src/services/systemMessageService.ts
@@ -0,0 +1,17 @@
+import { ChatMessage } from "@/interfaces";
+import api from "./api";
+/**
+ * function to send system message
+ * @param {ChatMessage} message
+ */
+export async function sendSystemMessage(
+  databaseId: string,
+  collectionId: string,
+  message: any,
+) {
+  await api.createDocument(databaseId, collectionId, {
+    ...message,
+    senderID: "system",
+    read: true,
+  });
+}
