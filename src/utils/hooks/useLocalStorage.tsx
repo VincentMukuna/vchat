d565220197a5ea7b8c6a92bff89752c32b683173
Vincent Mukuna <mukunavincent28@gmail.com>
2024-03-22 00:27:04 +0300

    [feat] add use-local-storage hook

---
+++ src/utils/hooks/useLocalStorage.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+function useLocalStorage<T>(key: string, initialValue: T) {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    try {
+      window.localStorage.setItem(key, JSON.stringify(storedValue));
+    } catch (error) {
+      console.error(error);
+    }
+  }, [key, storedValue]);
+
+  const setValue = (value: T) => {
+    setStoredValue(value);
+  };
+
+  return [storedValue, setValue] as const;
+}
+
+export default useLocalStorage;
