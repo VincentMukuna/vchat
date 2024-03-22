3de16d3cfbdcfe6de9a28fe35c62aeb8d0ce1e1a
Vincent Mukuna <mukunavincent28@gmail.com>
2024-03-22 04:08:16 +0300

    [style] system message

--- src/features/Room/Messages/SystemMessage.tsx
+++ src/features/Room/Messages/SystemMessage.tsx
@@ -2,7 +2,7 @@ import { ChatMessage } from "@/interfaces";
 
 export default function SystemMessage({ message }: { message: ChatMessage }) {
   return (
-    <div className="self-center p-3 px-4 my-1 text-xs italic rounded-full dark:text-gray-300 bg-dark-tomato11/60 dark:bg-dark-tomato7">
+    <div className="max-w-[min(90vw,24rem)]  py-2 text-center w-fit px-4 mx-auto my-1 text-xs italic rounded-full dark:text-gray-300 bg-dark-tomato11/60 dark:bg-dark-grass8/70">
       {message.body}
     </div>
   );
