50c2bee775912cee3fa07190c05ff0c620806d0b
Vincent Mukuna <mukunavincent28@gmail.com>
2024-03-25 19:15:16 +0300

    Update Spinner color in Loading component

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -3,7 +3,7 @@ import { Spinner } from "@chakra-ui/react";
 const Loading = () => {
   return (
     <div className="flex items-center justify-center h-screen">
-      <Spinner color={"blue.300"} />
+      <Spinner />
     </div>
   );
 };
