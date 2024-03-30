710b0d969ac75627f8939e4aabd87cc7a1b9303c
Vincent Mukuna <mukunavincent28@gmail.com>
2024-03-30 03:18:49 +0300

    [refactor] compose message component

---
+++ src/features/Room/Messages/MessageOptions.tsx
@@ -0,0 +1,77 @@
+import { confirmAlert } from "@/components/Alert/alertStore";
+import { DeleteIcon, PencilIcon } from "@/components/Icons";
+import { modal } from "@/components/VModal";
+import {
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+  Portal,
+} from "@chakra-ui/react";
+import {
+  ArrowUturnRightIcon,
+  EllipsisHorizontalIcon,
+} from "@heroicons/react/20/solid";
+import ForwardMessagesModal from "../ForwardMessagesModal";
+import EditMessageForm from "./EditMessageModal";
+import { useMessageContext } from "./Message";
+
+const MessageOptions = () => {
+  const { handleDelete, message } = useMessageContext();
+  return (
+    <Menu isLazy>
+      <MenuButton
+        as={IconButton}
+        aria-label="message options"
+        variant={"ghost"}
+        size={"xs"}
+        icon={<EllipsisHorizontalIcon className="w-4 h-4" />}
+      />
+      <Portal>
+        <MenuList>
+          <MenuItem
+            icon={<PencilIcon className="w-3" />}
+            command="⌘E"
+            onClick={(e) => {
+              e.stopPropagation();
+              modal(<EditMessageForm message={message} />);
+            }}
+          >
+            Edit Message
+          </MenuItem>
+          <MenuItem
+            icon={<ArrowUturnRightIcon className="w-3 h-3" />}
+            command="⌘F"
+            onClick={(e) => {
+              e.stopPropagation();
+              modal(<ForwardMessagesModal selectedMessages={[message]} />);
+            }}
+          >
+            Forward Message
+          </MenuItem>
+          <MenuDivider />
+          <MenuItem
+            icon={<DeleteIcon className="w-3" />}
+            command="⌘D"
+            onClick={(e) => {
+              confirmAlert({
+                message: "Delete this message? This action is irreversible",
+                title: "Delete message",
+                confirmText: "Delete",
+                onConfirm: () => handleDelete(message.$id),
+              });
+              e.stopPropagation();
+            }}
+            color={"red.500"}
+          >
+            Delete
+          </MenuItem>
+        </MenuList>
+      </Portal>
+    </Menu>
+  );
+};
+
+export default MessageOptions;
