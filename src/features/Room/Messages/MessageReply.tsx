c55f289b8cc69893510de8159ed5c5071e7e54cf
Vincent Mukuna <mukunavincent28@gmail.com>
2024-04-02 19:50:12 +0300

    Update MessageReply styling

--- src/features/Room/Messages/MessageReply.tsx
+++ src/features/Room/Messages/MessageReply.tsx
@@ -24,9 +24,9 @@ const MessageReply = ({ message }: { message: ChatMessage }) => {
 
           replyMessageElement?.focus();
         }}
-        className="sm:max-w-[22rem] max-w-[80vw] border-indigo-500 text-gray-500 mt-2 dark:text-gray-400 py-1 overflow-hidden border-s-4 ps-4"
+        className="sm:max-w-[22rem] max-w-[80vw] border-indigo-500 text-gray-500 mt-2 dark:text-gray-400 py-1 overflow-hidden border-s-4 ps-2"
       >
-        <span className="line-clamp-2">{replying?.body}</span>
+        <span className="text-xs line-clamp-2">{replying?.body}</span>
       </div>
     )
   );
