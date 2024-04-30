665d76cfcc46a158287fed75b3ca4fa85049a83e
Vincent Mukuna <mukunavincent28@gmail.com>
2024-04-30 04:20:35 +0300

    Fix bug in ForwardMessagesModal component

--- src/features/Room/ForwardMessagesModal.tsx
+++ src/features/Room/ForwardMessagesModal.tsx
@@ -31,6 +31,7 @@ export default function ForwardMessagesModal({
   );
   const {
     conversationsData: { conversations },
+    selectedChat,
   } = useChatsContext();
   if (!conversations || !currentUserDetails) return null;
   async function handleForwardMessages() {
@@ -38,6 +39,10 @@ export default function ForwardMessagesModal({
       if (!selectedChatId) return;
       if (!currentUserDetails) return;
 
+      updateRoomMessages([], {
+        revalidate: true,
+        optimisticData: (data) => [...selectedMessages, ...(data || [])],
+      });
       const selectedChatDetails = conversations.find(
         (convo) => convo.$id === selectedChatId,
       )!;
@@ -83,90 +88,105 @@ export default function ForwardMessagesModal({
           messages,
         );
       }
-      updateRoomMessages([], {
-        revalidate: true,
-        optimisticData: (data) => [...selectedMessages, ...(data || [])],
-      });
+
       toast.success("Messages forwarded successfully");
     } catch (error) {
+      updateRoomMessages(
+        (data: ChatMessage[]) => {
+          if (!data) return [];
+          return data.filter(
+            (message) =>
+              !selectedMessages.some(
+                (selectedMessage) => selectedMessage.$id === message.$id,
+              ),
+          );
+        },
+        { revalidate: true },
+      );
       toast.error("Failed to forward messages");
     }
   }
 
   return (
     <>
-      <ModalHeader>Forward Messages</ModalHeader>
+      <ModalHeader>{`Forward ${selectedMessages.length} Message(s) `}</ModalHeader>
       <ModalCloseButton />
       <ModalBody>
-        <h2>Select chat to forward to: </h2>
-        <ul className="mt-2 flex max-h-[14rem] flex-col gap-1 overflow-y-auto">
-          {conversations.map((conversation) => {
-            const isGroup =
-              conversation.$collectionId === SERVER.COLLECTION_ID_GROUPS;
-            const isPersonal =
-              !isGroup &&
-              conversation.participants.every(
-                (participant: any) =>
-                  participant.$id === currentUserDetails.$id,
+        <h2>Select chat to forward to:</h2>
+        <ul className="mt-2 flex max-h-[14rem] flex-col gap-1 overflow-y-auto p-1">
+          {conversations
+            .filter((c) => c.$id !== selectedChat?.$id)
+            .map((conversation) => {
+              const isGroup =
+                conversation.$collectionId === SERVER.COLLECTION_ID_GROUPS;
+              const isPersonal =
+                !isGroup &&
+                conversation.participants.every(
+                  (participant: any) =>
+                    participant.$id === currentUserDetails.$id,
+                );
+
+              let otherParticipant: IUserDetails;
+              if (!isGroup) {
+                otherParticipant = isPersonal
+                  ? currentUserDetails
+                  : conversation.participants.find(
+                      (participant: any) =>
+                        participant.$id !== currentUserDetails.$id,
+                    );
+              }
+
+              return (
+                <li
+                  key={conversation.$id}
+                  className={`flex items-center gap-2 ${
+                    selectedChatId === conversation.$id
+                      ? "bg-slate-200 ring-1 dark:bg-slate-800"
+                      : ""
+                  } cursor-pointer rounded-md p-2 transition-colors hover:bg-slate-200 dark:hover:bg-slate-800`}
+                  onClick={() => {
+                    if (selectedChatId === conversation.$id) {
+                      setSelectedChatId("");
+                      return;
+                    }
+                    setSelectedChatId(conversation.$id);
+                  }}
+                >
+                  <Avatar
+                    size={"md"}
+                    icon={<UserIcon className="h-5 w-5" />}
+                    src={
+                      isGroup
+                        ? conversation.avatarURL
+                        : otherParticipant!.avatarURL
+                    }
+                  />
+                  <div className="flex flex-col ">
+                    <span>
+                      {isGroup
+                        ? conversation.name
+                        : isPersonal
+                        ? "You"
+                        : otherParticipant!.name}
+                    </span>
+                    <span className="line-clamp-1 text-xs italic">
+                      {isGroup
+                        ? conversation.description
+                        : otherParticipant!.about || "about"}
+                    </span>
+                  </div>
+                </li>
               );
-
-            let otherParticipant: IUserDetails;
-            if (!isGroup) {
-              otherParticipant = isPersonal
-                ? currentUserDetails
-                : conversation.participants.find(
-                    (participant: any) =>
-                      participant.$id !== currentUserDetails.$id,
-                  );
-            }
-
-            return (
-              <li
-                key={conversation.$id}
-                className={`flex items-center gap-2 ${
-                  selectedChatId === conversation.$id ? "bg-slate-800" : ""
-                } cursor-pointer rounded-md p-2 transition-colors hover:bg-slate-800`}
-                onClick={() => {
-                  if (selectedChatId === conversation.$id) {
-                    setSelectedChatId("");
-                    return;
-                  }
-                  setSelectedChatId(conversation.$id);
-                }}
-              >
-                <Avatar
-                  size={"md"}
-                  icon={<UserIcon className="h-5 w-5" />}
-                  src={
-                    isGroup
-                      ? conversation.avatarURL
-                      : otherParticipant!.avatarURL
-                  }
-                />
-                <div className="flex flex-col ">
-                  <span>
-                    {isGroup
-                      ? conversation.name
-                      : isPersonal
-                      ? "You"
-                      : otherParticipant!.name}
-                  </span>
-                  <span className="line-clamp-1 text-xs italic">
-                    {isGroup
-                      ? conversation.description
-                      : otherParticipant!.about || "about"}
-                  </span>
-                </div>
-              </li>
-            );
-          })}
+            })}
         </ul>
       </ModalBody>
       <ModalFooter>
-        <Button variant="ghost" mr={3}>
+        <Button variant="ghost" mr={3} onClick={() => onClose()}>
           Cancel
         </Button>
         <Button
+          isDisabled={selectedChatId.length === 0}
+          title={selectedChatId.length === 0 ? "Select chat to forward to" : ""}
           colorScheme="blue"
           color={"gray.100"}
           onClick={() => {
