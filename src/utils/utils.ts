34a97b573086e57bcc19ec3cf04efb8d7dbee109
Vincent Mukuna <mukunavincent28@gmail.com>
2024-06-07 22:16:28 +0300

    add fallback to message copying

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -145,3 +145,30 @@ export function removeDuplicates<T>(arr: Array<T>): Array<T> {
 export function insertCharacter(str: string, char: string, position: number) {
   return str.slice(0, position) + char + str.slice(position);
 }
+
+function fallbackCopyTextToClipboard(text: string) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    document.execCommand("copy");
+  } catch (err) {}
+
+  document.body.removeChild(textArea);
+}
+export async function copyTextToClipboard(text: string) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  await navigator.clipboard.writeText(text);
+}
