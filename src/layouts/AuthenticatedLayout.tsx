33496bfc9aa38169296b228c2f355ce3f6b12b65
Vincent Mukuna <mukunavincent28@gmail.com>
2026-06-04 22:18:51 +0300

    Update AuthenticatedLayout to use dynamic background color based on color mode

--- src/layouts/AuthenticatedLayout.tsx
+++ src/layouts/AuthenticatedLayout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import MobileNav from "../components/Navbar/MobileNav";
 import Navbar from "../components/Navbar/Navbar";
+import { useColorModeValue } from "@chakra-ui/react";
+import { blueDark, gray } from "@radix-ui/colors";
 
 interface AuthenticatedLayoutProps {
   children: React.ReactNode;
@@ -11,9 +13,11 @@ function AuthenticatedLayout({
   children,
   hideMobileNav = false,
 }: AuthenticatedLayoutProps) {
+   const navBackground = useColorModeValue(gray.gray2, blueDark.blue1);
   return (
     <div
-      className={`fixed inset-0 flex flex-col-reverse md:flex-row md:pb-0 ${
+      style={{ backgroundColor: navBackground }}
+      className={`fixed inset-0 flex flex-col-reverse md:flex-row md:pb-0 bg-gray2 dark:bg-dark-blue1 ${
         hideMobileNav ? "pb-0" : "pb-[4.5rem]"
       }`}
     >
